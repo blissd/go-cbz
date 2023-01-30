ce25550a75888365cac7bc99689a9508c1f93dee
David Bliss <github@davidbliss.name>
2023-01-30 07:31:15 +0000

    Make most struct fields pointers

--- main.go
+++ main.go
@@ -35,59 +35,59 @@ type Rating float32
 type ComicPageType string
 
 type ComicPageInfo struct {
-	Image       int
-	Type        ComicPageType
-	DoublePage  bool
-	ImageSize   int64
-	Key         string
-	Bookmark    string
-	ImageWidth  int
-	ImageHeight int
+	Image       int            `xml:",attr"`
+	Type        *ComicPageType `xml:",attr"`
+	DoublePage  *bool          `xml:",attr"`
+	ImageSize   *int64         `xml:",attr"`
+	Key         *string        `xml:",attr"`
+	Bookmark    *string        `xml:",attr"`
+	ImageWidth  *int           `xml:",attr"`
+	ImageHeight *int           `xml:",attr"`
 }
 
 type ArrayOfComicPageInfo []ComicPageInfo
 
 type ComicInfo struct {
-	Title               string
-	Series              string
-	Number              string
-	Count               int
-	Volume              int
-	AlternativeSeries   string
-	AlternativeNumber   string
-	AlternativeCount    int
-	Summary             string
-	Notes               string
-	Year                int
-	Month               int
-	Day                 int
-	Writer              string
-	Penciller           string
-	Inker               string
-	Colorist            string
-	Letterer            string
-	CoverArtist         string
-	Editor              string
-	Publisher           string
-	Imprint             string
-	Genre               string
-	Web                 string
-	PageCount           int
-	LanguageISO         string
-	Format              string
-	BlackAndWhite       YesNo
-	Manga               Manga
-	Characters          string
-	Teams               string
-	Locations           string
-	ScanInformation     string
-	StoryArc            string
-	SeriesGroup         string
-	AgeRating           AgeRating
+	Title               *string
+	Series              *string
+	Number              *string
+	Count               *int
+	Volume              *int
+	AlternativeSeries   *string
+	AlternativeNumber   *string
+	AlternativeCount    *int
+	Summary             *string
+	Notes               *string
+	Year                *int
+	Month               *int
+	Day                 *int
+	Writer              *string
+	Penciller           *string
+	Inker               *string
+	Colorist            *string
+	Letterer            *string
+	CoverArtist         *string
+	Editor              *string
+	Publisher           *string
+	Imprint             *string
+	Genre               *string
+	Web                 *string
+	PageCount           *int
+	LanguageISO         *string
+	Format              *string
+	BlackAndWhite       *YesNo
+	Manga               *Manga
+	Characters          *string
+	Teams               *string
+	Locations           *string
+	ScanInformation     *string
+	StoryArc            *string
+	SeriesGroup         *string
+	AgeRating           *AgeRating
 	Pages               ArrayOfComicPageInfo
-	CommunityRating     Rating
-	MainCharacterOrTeam string
-	Review              string
+	CommunityRating     *Rating
+	MainCharacterOrTeam *string
+	Review              *string
 }
 
 func showComicInfo(file *zip.File) error {
