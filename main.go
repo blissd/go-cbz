648ffa633c6f16fd1a9df57d2a8145268a3aeb98
David Bliss <github@davidbliss.name>
2023-01-30 07:20:51 +0000

    Add ComicInfo struts

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"archive/zip"
+	"encoding/xml"
 	"fmt"
+	"io"
 	"log"
 )
 
@@ -15,5 +17,105 @@ func main() {
 
 	for _, file := range input.File {
 		fmt.Println("name: ", file.Name)
+		if file.Name == "ComicInfo.xml" {
+			fmt.Println("Has metadata")
+			err := showComicInfo(file)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
 	}
+}
+
+type YesNo string
+type AgeRating string
+type Manga string
+type Rating float32
+
+type ComicPageType string
+
+type ComicPageInfo struct {
+	Image       int
+	Type        ComicPageType
+	DoublePage  bool
+	ImageSize   int64
+	Key         string
+	Bookmark    string
+	ImageWidth  int
+	ImageHeight int
+}
+
+type ArrayOfComicPageInfo []ComicPageInfo
+
+type ComicInfo struct {
+	Title               string
+	Series              string
+	Number              string
+	Count               int
+	Volume              int
+	AlternativeSeries   string
+	AlternativeNumber   string
+	AlternativeCount    int
+	Summary             string
+	Notes               string
+	Year                int
+	Month               int
+	Day                 int
+	Writer              string
+	Penciller           string
+	Inker               string
+	Colorist            string
+	Letterer            string
+	CoverArtist         string
+	Editor              string
+	Publisher           string
+	Imprint             string
+	Genre               string
+	Web                 string
+	PageCount           int
+	LanguageISO         string
+	Format              string
+	BlackAndWhite       YesNo
+	Manga               Manga
+	Characters          string
+	Teams               string
+	Locations           string
+	ScanInformation     string
+	StoryArc            string
+	SeriesGroup         string
+	AgeRating           AgeRating
+	Pages               ArrayOfComicPageInfo
+	CommunityRating     Rating
+	MainCharacterOrTeam string
+	Review              string
+}
+
+func showComicInfo(file *zip.File) error {
+	r, err := file.Open()
+	if err != nil {
+		return fmt.Errorf("failed to open zip %s for reading: %w", file.Name, err)
+	}
+	defer r.Close()
+
+	bs, err := io.ReadAll(r)
+	if err != nil {
+		return fmt.Errorf("failed to read file %s: %w", file.Name, err)
+	}
+
+	info := ComicInfo{}
+	err = xml.Unmarshal(bs, &info)
+	if err != nil {
+		return fmt.Errorf("failed to XML unmarshal %s: %w", file.Name, err)
+	}
+
+	//fmt.Println("raw info:", info)
+
+	marshal, err := xml.MarshalIndent(&info, "", " ")
+	if err != nil {
+		return fmt.Errorf("failed to XML marshal %s: %w", file.Name, err)
+	}
+
+	fmt.Println("marshalled XML:", string(marshal))
+
+	return nil
 }
